396d403ec307ab407e72db28f5152756b47556cf
eliezerraj <eliezerraj@gmail.com>
2026-02-26 18:30:32 -0300

    feat:minor fix

--- assets/siege/02_order.sh
+++ assets/siege/02_order.sh
@@ -2,9 +2,10 @@
 
 # variabels
 
-export AUTH_TOKEN=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJpc3MiOiJsYW1iZGEtZ28taWRlbnRpZHkiLCJ2ZXJzaW9uIjoiMS4xIiwidXNlcm5hbWUiOiJhZG1pbi0xMDEiLCJqd3RfaWQiOiJiZGZmM2Q1NS03MzQ5LTQyZGItYWJlZS0yZmE1ODc4MWE1MmUiLCJraWQiOiJhdXRoLWtleTpzZXJ2ZXItcHVibGljLmtleSIsInRpZXIiOiJ0aWVyMSIsImFwaV9hY2Nlc3Nfa2V5IjoiQVBJX0FDQ0VTU19LRVlfQURNSU5fMDAxIiwic2NvcGUiOlsidG9vbDppbmZvIiwidG9vbDpoZWFsdGgiLCJ0b29sOmdldF9wcm9kdWN0IiwiYWRtaW4iXSwiZXhwIjoxNzcxODc4NTIyfQ.ADtVMBRmCXWg5fAyvlOSUDOmFPkukhPzRrAdGlojLlE
+export AUTH_TOKEN=
 
 export URL_HOST=https://go-api-global.architecture.caradhras.io/order
+# export URL_HOST=http://localhost:7004
 
 #-----------------------------------------------------
 URL_GET="${URL_HOST}/info"
@@ -37,11 +38,11 @@ else
 fi
 
 #------------------------------------------
-RANDOM_INV=$((RANDOM % 99 + 1))
-RANDOM_VAL=$((RANDOM % 9 + 1))
+RANDOM_INV=$((RANDOM % 999 + 1))
+RANDOM_VAL=$((RANDOM % 99 + 1))
 
 URL_POST="${URL_HOST}/order"
-PAYLOAD='{"user_id": "siege","currency": "BRL","address": "st. a","cart":{"user_id": "eliezer","cart_item":[{"product": {"sku": "JUICE-'$RANDOM_INV'"},"currency": "BRL","quantity": 1'${RANDOM_VAL}',"price": '${RANDOM_VAL}'},{"product": {"sku": "MOBILE-'$RANDOM_INV'"},"currency": "BRL","quantity": '${RANDOM_VAL}',"price": '${RANDOM_VAL}' }]}}'
+PAYLOAD='{"user_id": "siege","currency": "BRL","address": "st. a","cart":{"user_id": "eliezer","cart_item":[{"product": {"sku": "BEER-'$RANDOM_INV'"},"currency": "BRL","quantity": 1'${RANDOM_VAL}',"price": '${RANDOM_VAL}'},{"product": {"sku": "MOBILE-'$RANDOM_INV'"},"currency": "BRL","quantity": '${RANDOM_VAL}',"price": '${RANDOM_VAL}' }]}}'
 
 STATUS_CODE=$(curl -s -w " HTTP:%{http_code}" "$URL_POST" \
 	--header "Content-Type: application/json" \
